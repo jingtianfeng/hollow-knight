a00cd2769f02e8fa8e39a68273abb470803c306c
jingtianfeng <jingtianfeng@hotmail.com>
2021-07-19 00:57:22 -0400

    Refactor Checkout

--- src/App.js
+++ src/App.js
@@ -15,39 +15,14 @@ import vinylOriginal2 from './img/vinyl/original-2.png'
 import myla from './img/myla.png'
 import NavTop from "./NavTop";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 function App() {
     return (
         <>
             <NavTop/>
             <Cart/>
-            <div id="checkout" className="modal fade"
-                 aria-hidden="true" aria-labelledby="checkout" tabIndex="-1">
-                <div className="modal-dialog modal-xl">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h2 className="modal-title">
-                                Checkout
-                            </h2>
-                            <button type="button"
-                                    className="btn-close"
-                                    data-bs-dismiss="modal"
-                                    aria-label="Close"/>
-                        </div>
-                        <div className="modal-body">
-                            Hide this modal and show the first with the button below.
-                        </div>
-                        <div className="modal-footer">
-                            <button className="btn btn-primary"
-                                    data-bs-toggle="offcanvas"
-                                    data-bs-target="#cart"
-                                    data-bs-dismiss="modal">
-                                back to cart
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Checkout/>
             <header id="header"
                     className="border-bottom py-5">
                 <div id="header__container" className="container">
---
+++ src/Checkout.js
@@ -0,0 +1,33 @@
+function Checkout() {
+    return (
+        <div id="checkout" className="modal fade"
+             aria-hidden="true" aria-labelledby="checkout" tabIndex="-1">
+            <div className="modal-dialog modal-xl">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h2 className="modal-title">
+                            Checkout
+                        </h2>
+                        <button type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"/>
+                    </div>
+                    <div className="modal-body">
+                        Hide this modal and show the first with the button below.
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-primary"
+                                data-bs-toggle="offcanvas"
+                                data-bs-target="#cart"
+                                data-bs-dismiss="modal">
+                            back to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
