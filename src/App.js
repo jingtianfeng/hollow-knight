243ee024654dcd40627f2c984bc5603146d33b9e
jingtianfeng <jingtianfeng@hotmail.com>
2021-07-19 00:54:05 -0400

    Refactor Cart

--- src/App.js
+++ src/App.js
@@ -14,33 +14,13 @@ import vinylGN2 from './img/vinyl/gods-nightmares-2.png'
 import vinylOriginal2 from './img/vinyl/original-2.png'
 import myla from './img/myla.png'
 import NavTop from "./NavTop";
+import Cart from "./Cart";
 
 function App() {
     return (
         <>
             <NavTop/>
-            <div id="cart" className="offcanvas offcanvas-end"
-                 data-bs-backdrop="false" data-bs-scroll="true"
-                 aria-hidden="true" aria-labelledby="cart" tabIndex="-1">
-                <div className="offcanvas-header">
-                    <h2 className="offcanvas-title">
-                        Cart
-                    </h2>
-                    <button type="button"
-                            className="btn-close text-reset"
-                            data-bs-dismiss="offcanvas"
-                            aria-label="close"/>
-                </div>
-                <div className="offcanvas-body">
-                    Your cart is empty.
-                    <button className="btn btn-primary"
-                            data-bs-toggle="modal"
-                            data-bs-target="#checkout"
-                            data-bs-dismiss="offcanvas">
-                        checkout
-                    </button>
-                </div>
-            </div>
+            <Cart/>
             <div id="checkout" className="modal fade"
                  aria-hidden="true" aria-labelledby="checkout" tabIndex="-1">
                 <div className="modal-dialog modal-xl">
---
+++ src/Cart.js
@@ -0,0 +1,28 @@
+function Cart() {
+    return (
+        <div id="cart" className="offcanvas offcanvas-end"
+             data-bs-backdrop="false" data-bs-scroll="true"
+             aria-hidden="true" aria-labelledby="cart" tabIndex="-1">
+            <div className="offcanvas-header">
+                <h2 className="offcanvas-title">
+                    Cart
+                </h2>
+                <button type="button"
+                        className="btn-close text-reset"
+                        data-bs-dismiss="offcanvas"
+                        aria-label="close"/>
+            </div>
+            <div className="offcanvas-body">
+                Your cart is empty.
+                <button className="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#checkout"
+                        data-bs-dismiss="offcanvas">
+                    checkout
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
