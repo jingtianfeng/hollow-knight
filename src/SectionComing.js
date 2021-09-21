4d652b304beddee94a6ed81642b5c31cb45bbd4f
jingtianfeng <jingtianfeng@hotmail.com>
2021-09-20 22:37:54 -0400

    Fix SectionComing: heading, card container width

--- src/SectionComing.js
+++ src/SectionComing.js
@@ -1,22 +1,16 @@
 import vinylGN2 from "./img/vinyl/gods-nightmares-2.png";
 import vinylOriginal2 from "./img/vinyl/original-2.png";
-import marker from "./img/marker3.png";
 
 function SectionComing() {
     return (
         <section id="section-coming"
                  className="py-6 border-bottom">
             <div className="container">
-                <h2 className="text-center">
+                <h2 className="text-center fs-1 letter-spacing-7 mb-5">
                     Coming Soon
                 </h2>
-                <img src={marker} alt="divider"
-                     className="d-block mx-auto"
-                     style={{width: 24, height: 24, opacity: 0.875}}/>
-                <p className="mt-2 text-center">
-                    (placeholder)
-                </p>
-                <div className="row row-cols-1 row-cols-md-2 gy-5 gx-md-5 text-center">
+                <div className="row row-cols-1 row-cols-md-2 gy-5 gx-md-5 text-center mx-auto"
+                     style={{maxWidth: 900}}>
                     {[
                         {
                             imgSrc: `${vinylGN2}`,
@@ -34,9 +28,10 @@ function SectionComing() {
                                 <div className="card bg-transparent">
                                     <img src={imgSrc}
                                          alt="vinyl - original soundtrack"
-                                         className="card-img"/>
+                                         className="card-img"
+                                         style={{marginTop: -50}}/>
                                     <div className="card-img-overlay d-flex flex-column justify-content-end">
-                                        <h3 className="card-text h5 letter-spacing-5">
+                                        <h3 className="card-text h6 letter-spacing-4">
                                             {title}
                                         </h3>
                                         <p className="mb-0 mb-lg-5 text-uppercase text-white-50 letter-spacing-5"
