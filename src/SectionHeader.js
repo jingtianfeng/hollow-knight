809a12a198ee530ae1059a681bafca30e46c8478
jingtianfeng <jingtianfeng@hotmail.com>
2021-09-22 22:43:00 -0400

    Fix SectionHeader: various

--- src/SectionHeader.js
+++ src/SectionHeader.js
@@ -4,7 +4,7 @@ import banner from "./img/banner.png";
 function SectionHeader() {
     return (
         <header id="header"
-                className="border-bottom py-5"
+                className="border-bottom py-6"
                 style={{
                     backgroundImage: `url(${banner})`,
                     backgroundSize: "cover",
@@ -15,9 +15,9 @@ function SectionHeader() {
                      id="header__container__logo"
                      className="d-block img-fluid mx-auto mb-3"/>
                 <h1 id="header__container__title"
-                    className="h1 text-white text-center"
+                    className="h1 text-white text-center letter-spacing-3"
                     style={{
-                        opacity: 0.95,
+                        opacity: 0.875,
                         textShadow: "-1px 0 #161a44, 0 1px #161a44, 1px 0 #161a44, 0 -1px #161a44"
                     }}>
                     Merch Store
